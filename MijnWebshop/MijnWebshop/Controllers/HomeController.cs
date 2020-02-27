936f2e374871868d542c3c3e9e3c81c510d2f474
Ludwig Nassen <Ludwig.Nassen@kcst.be>
2020-02-27 16:44:56 +0100

    Aanmaken project

---
+++ MijnWebshop/MijnWebshop/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MijnWebshop.Models;
+
+namespace MijnWebshop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
